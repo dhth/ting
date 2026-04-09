eaf4008a4b216a66e571a643fc3df3d3cf246c71
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2026-04-09 22:26:43 +0200

    build: bump rodio to 0.22.2
    bump bytes to 1.11.1 to fix
    https://rustsec.org/advisories/RUSTSEC-2026-0007

--- src/audio/playback.rs
+++ src/audio/playback.rs
@@ -1,17 +1,17 @@
 use anyhow::Context;
-use rodio::{Decoder, OutputStreamBuilder, Sink};
+use rodio::{Decoder, DeviceSinkBuilder, Player};
 use std::io::Cursor;
 
 pub fn play_audio(sound_bytes: Vec<u8>) -> anyhow::Result<()> {
-    let stream_handle = OutputStreamBuilder::open_default_stream()
-        .context("couldn't open default output stream")?;
-    let sink = Sink::connect_new(stream_handle.mixer());
+    let sink =
+        DeviceSinkBuilder::open_default_sink().context("couldn't open default output stream")?;
+    let player = Player::connect_new(sink.mixer());
 
     let cursor = Cursor::new(sound_bytes);
     let source = Decoder::new(cursor).context("couldn't decode audio bytes")?;
 
-    sink.append(source);
-    sink.sleep_until_end();
+    player.append(source);
+    player.sleep_until_end();
 
     Ok(())
 }
